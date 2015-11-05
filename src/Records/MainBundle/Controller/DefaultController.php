c1d6eda285205cb51f5e877c86d3dd58ea4d9e73
Jaron <jaronjking@gmail.com>
2015-11-05 08:26:09 -0800

    Persisting records to archives, currently not working.

--- src/Records/MainBundle/Controller/DefaultController.php
+++ src/Records/MainBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace Records\MainBundle\Controller;
 
+use Records\MainBundle\Entity\Archive;
 use Records\MainBundle\Entity\Record;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -12,9 +13,12 @@ class DefaultController extends Controller
         return $this->render('MainBundle:Default:index.html.twig');
     }
 
+    /**
+     * Every 22 minutes a student takes a type of test and submits their score.
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function createAction()
     {
-        //22 Records Every Minute
         $em = $this->getDoctrine()->getManager();
         $records = array();
         for($i = 0; $i < 22; $i++){
@@ -34,14 +38,16 @@ class DefaultController extends Controller
         ));
     }
 
+    /**
+     * Every minute the score keeper records the total amount of students who took a type of test
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
     public function moveAction()
     {
         $em = $this->getDoctrine()->getManager();
         $archives = array();
         $records = $em->getRepository('MainBundle:Record')->findBy(
-            array(
-                'archived' => false
-            ),
+            array( 'archived' => false ),
             array( 'dateCreated' => 'desc'),
             22 ,0
         );
@@ -49,8 +55,23 @@ class DefaultController extends Controller
             $dateCreated = $value->getDateCreated()->format('Y-m-d H:i:s');
             $type = $value->getType();
             $score = $value->getScore();
-            $archives[$dateCreated][$type][] = $score;
+            if (!isset($archives[$dateCreated])) $archives[$dateCreated] = array();
+            if (!isset($archives[$dateCreated][$type])) $archives[$dateCreated][$type] = 0;
+
+            $archives[$dateCreated][$type] ++;
         }
+
+        foreach( $archives as $dateCreated => $value) {
+            foreach ($value as $type => $amountOfStudents ) {
+                $archive = new Archive();
+                $archive->setDateCreated($dateCreated);
+                $archive->setType('Total_' . $type);
+                $archive->setScore($amountOfStudents);
+                $em->persist($archive);
+            }
+        }
+        $em->flush();
+
         return $this->render('MainBundle:Default:moveRecord.html.twig', array(
             'records' => $records,
             'archives' => $archives,
