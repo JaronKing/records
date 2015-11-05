d583a8687c72437dfe845e0221fcc18f5904e605
Jaron <jaronjking@gmail.com>
2015-11-05 10:11:55 -0800

    Full create and move student records in one action.

--- src/Records/MainBundle/Controller/DefaultController.php
+++ src/Records/MainBundle/Controller/DefaultController.php
@@ -77,4 +77,56 @@ class DefaultController extends Controller
             'archives' => $archives,
         ));
     }
+
+    function createAndMoveAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $records = array();
+        for($i = 0; $i < 22; $i++){
+            $record = new Record();
+            $record->setDateCreated(new \DateTime('now'));
+            $record->setName(uniqid());
+            $record->setScore(rand(1,100));
+            $record->setType(rand(1,10));
+            $record->setArchived(false);
+            $records[] = $record;
+            $em->persist($record);
+        }
+        $em->flush();
+
+        sleep(1);
+
+        $archives = array();
+        $records2 = $em->getRepository('MainBundle:Record')->findBy(
+            array( 'archived' => false ),
+            array( 'dateCreated' => 'desc'),
+            22 ,0
+        );
+        foreach( $records2 as $key => $value ) {
+            $dateCreated = $value->getDateCreated()->format('Y-m-d H:i:s');
+            $type = $value->getType();
+            $score = $value->getScore();
+            if (!isset($archives[$dateCreated])) $archives[$dateCreated] = array();
+            if (!isset($archives[$dateCreated][$type])) $archives[$dateCreated][$type] = 0;
+
+            $archives[$dateCreated][$type] ++;
+        }
+
+        foreach( $archives as $dateCreated => $value) {
+            foreach ($value as $type => $amountOfStudents ) {
+                $archive = new Archive();
+                $archive->setDateCreated(new \DateTime('now'));
+                $archive->setType('Total_' . $type);
+                $archive->setScore($amountOfStudents);
+                $em->persist($archive);
+            }
+        }
+        $em->flush();
+
+        return $this->render('MainBundle:Default:createAndMoveRecord.html.twig', array(
+            'records' => $records,
+            'records2' => $records2,
+            'archives' => $archives,
+        ));
+    }
 }
